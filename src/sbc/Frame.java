16c1476a468d6e1ba8e137b96a37431f4473e132
TRAUT Anthony <traut2u@tncy-1-18-05.telecomnancy.site.univ-lorraine.fr>
2018-03-12 11:32:44 +0100

    rien

--- src/sbc/Frame.java
+++ src/sbc/Frame.java
@@ -1,6 +1,6 @@
 package sbc;
 
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -8,15 +8,44 @@ public class Frame extends JFrame{
 	
 	public Frame(){
 		this.setTitle("MyVowl");
-		this.setSize(700,600);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-	    this.setVisible(true);
+		this.setSize(900,800);
 	    
 	    JPanel topPanel = new JPanel();
+	    topPanel.setLayout(new BorderLayout());
+	    topPanel.setPreferredSize(new Dimension(900, 50));
+	    topPanel.setBackground(Color.LIGHT_GRAY);
+	    
+	    JPanel endpointPanel = new JPanel();
+	    endpointPanel.setLayout(new GridBagLayout());
+	    endpointPanel.setBackground(Color.LIGHT_GRAY);
+	    endpointPanel.setPreferredSize(new Dimension(topPanel.getPreferredSize().width/3,50));
+	    JTextField endpoint = new JTextField();
+	    endpoint.setPreferredSize(new Dimension(80, 20));
+	    endpoint.setMaximumSize(new Dimension(80, 20));
+	    endpointPanel.add(endpoint);
+	    
+	    JPanel classPanel = new JPanel();
+	    classPanel.setBackground(Color.LIGHT_GRAY);
+	    classPanel.setPreferredSize(new Dimension(topPanel.getPreferredSize().width/3, 50));
+	    
+	    JPanel propertyPanel = new JPanel();
+	    propertyPanel.setBackground(Color.LIGHT_GRAY);
+	    propertyPanel.setPreferredSize(new Dimension(topPanel.getPreferredSize().width/3,50));
+	    
+	    topPanel.add(endpointPanel, BorderLayout.WEST);
+	    topPanel.add(classPanel);
+	    topPanel.add(propertyPanel, BorderLayout.EAST);
 	    
 	    JPanel midPanel = new JPanel();
 	    midPanel.setBackground(Color.WHITE);
-	}
-
+	    
+	    this.getContentPane().setLayout(new BorderLayout());
+	    this.getContentPane().add(topPanel, BorderLayout.NORTH);
+	    this.getContentPane().add(midPanel, BorderLayout.CENTER);
+	    
+	    this.setResizable(false);
+	    this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+	    this.setVisible(true);
+	    }
 }
