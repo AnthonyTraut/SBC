417b42e585ea6ea75e57184932a89746a8e4c617
SCHILD Erwan <schild8u@tncy-1-18-06.telecomnancy.site.univ-lorraine.fr>
2018-03-12 11:51:32 +0100

    Graph

--- src/sbc/GraphCreator.java
+++ src/sbc/GraphCreator.java
@@ -1,10 +1,15 @@
 package sbc;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Paint;
+import java.awt.Stroke;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.apache.commons.collections15.Transformer;
 import org.apache.jena.query.ResultSet;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
@@ -12,6 +17,8 @@ import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
 
 public class GraphCreator {
 
@@ -20,28 +27,41 @@ public class GraphCreator {
 		
 	}
 	
-	public Graph<Node,Vertex> create(ResultSet res) {
+	public static Graph<Node,Vertex> create(ResultSet res) {
 		Graph<Node,Vertex> graph = new DirectedSparseMultigraph<Node,Vertex>();
 				
-		return graph;
-	}
-	
-	public Graph<Node,Vertex> test(ResultSet res) {
-		Graph<Node,Vertex> graph = new DirectedSparseMultigraph<Node,Vertex>();
-		
 		graph.addEdge(new Vertex("link"), new Node("n1"), new Node("n2"));
-		
 		return graph;
 	}
 	
-	public JPanel visualization(Graph<Node,Vertex> g) {
+	public static JPanel visualization(Graph<Node,Vertex> g) {
 		Layout<Integer, String> layout = new CircleLayout(g);
 		layout.setSize(new Dimension(300,300));
 		BasicVisualizationServer<Integer,String> vv = new BasicVisualizationServer<Integer,String>(layout);
 		vv.setPreferredSize(new Dimension(350,350));
-		JPanel panel = new JPanel();	
+		
+		Transformer<Integer,Paint> vertexPaint = new Transformer<Integer,Paint>() {
+			public Paint transform(Integer i) {
+				return Color.GREEN;
+			}
+		};
+		
+		float dash[] = {10.0f};
+		final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+		Transformer<String, Stroke> edgeStrokeTransformer = new Transformer<String, Stroke>() {
+			public Stroke transform(String s) {
+				return edgeStroke;
+			}
+		};
+		
+		vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+		vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+		vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
+		
+		JPanel panel = new JPanel();
 		panel.add(vv);
 		return panel;
-		
 	}
 }
