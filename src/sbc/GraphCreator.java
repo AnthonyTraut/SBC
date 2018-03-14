989d988ea269a87714e2f3886c2af7fa8a7289a7
Erwanschild <schild.erwan@yahoo.fr>
2018-03-15 00:21:21 +0100

    Dernières modifications

--- src/sbc/GraphCreator.java
+++ src/sbc/GraphCreator.java
@@ -33,7 +33,7 @@ public class GraphCreator {
 	private static HashMap<String, Vertex> nodeMap = new HashMap<String, Vertex>();
 		
 	/**
-	 * Permet de reset le graphe maintenu en m�moire
+	 * Permet de reset le graphe maintenu en memoire
 	 */
 	public static void resetGraph() {
 		GraphCreator.graph = new DirectedSparseMultigraph<Vertex,Edge>();//DelegateTree<Vertex, Edge>();
@@ -49,11 +49,12 @@ public class GraphCreator {
 	}
 	
 	/**
-	 * Ex�cute une query
+	 * Execute une query
 	 * @param query
 	 * @return
 	 */
 	public static QueryExecution execQuery(Query query) {
+		System.out.println(query);
 		QueryExecution queryexec = QueryExecutionFactory.sparqlService(sparqlService, query);
 		return queryexec;
 	}
@@ -63,8 +64,8 @@ public class GraphCreator {
 	 * @param classe
 	 * @param limit
 	 * @param A1 : Recherche relation T-Box avec autres classes
-	 * @param A2 : Recherche anc�tres et descendants
-	 * @param A3 : Recherche les pr�dicats les plus instanci�es
+	 * @param A2 : Recherche ancetres et descendants
+	 * @param A3 : Recherche les predicats les plus instanciees
 	 * @return
 	 */
 	public static JPanel createGraph_A(String classe, int limitA1, int limitA2, int limitA3, boolean A1, boolean A2, boolean A3) {
@@ -87,20 +88,20 @@ public class GraphCreator {
 	 * @param classe
 	 * @param limit
 	 */
-	public static void createGraph_A1(String classe, int limit) {
-		String s_classe = classe.toString().split("/")[classe.toString().split("/").length-1];
+	public static void createGraph_A1(String CLASS, int LIMIT) {
+		String s_classe = CLASS.toString().split("/")[CLASS.toString().split("/").length-1];
 		Vertex n_classeSource;
-		if (GraphCreator.nodeMap.containsKey(classe)) {
-			GraphCreator.nodeMap.get(classe).setColor(Color.BLUE);
-			n_classeSource = GraphCreator.nodeMap.get(classe);
+		if (GraphCreator.nodeMap.containsKey(CLASS)) {
+			GraphCreator.nodeMap.get(CLASS).setColor(Color.BLUE);
+			n_classeSource = GraphCreator.nodeMap.get(CLASS);
 		}
 		else {
-			n_classeSource = new Vertex(classe, s_classe, Color.BLUE);
-			GraphCreator.nodeMap.put(classe, n_classeSource);
+			n_classeSource = new Vertex(CLASS, s_classe, Color.BLUE);
+			GraphCreator.nodeMap.put(CLASS, n_classeSource);
 		}
 		
-		// Afficher CLASS ?predicat ?otherclass
-		QueryExecution queryexec = GraphCreator.execQuery(RequestBuilder.A1_R1(classe, limit));
+		// Afficher CLASS ?predicat ?class
+		QueryExecution queryexec = GraphCreator.execQuery(RequestBuilder.A1_R1(CLASS, LIMIT));
 		ResultSet res = queryexec.execSelect();
 		while (res.hasNext()) {
 			QuerySolution sol = res.next();
@@ -112,7 +113,7 @@ public class GraphCreator {
 			Edge predicat = new Edge(url_pre, s_pre, "T-Box");
 			
 			// La classe en relation via le predicat a la classe source
-			RDFNode cla = sol.get("otherclass");
+			RDFNode cla = sol.get("class");
 			String url_cla = cla.toString();
 			String s_cla = cla.toString().split("/")[cla.toString().split("/").length-1];
 			Vertex n_cla;
@@ -125,7 +126,7 @@ public class GraphCreator {
 				
 			}
 			
-			// V�rification que l'arc n'existe pas deja avant l'ajout
+			// Verification que l'arc n'existe pas deja avant l'ajout
 			boolean alreadyExist = false;
 			if (GraphCreator.graph.containsVertex(n_classeSource) && GraphCreator.graph.containsVertex(n_cla)) {
 				for(Edge e : GraphCreator.graph.findEdgeSet(n_classeSource, n_cla)) {
@@ -140,8 +141,8 @@ public class GraphCreator {
 			}
 		}
 		
-		// Afficher ?otherclass ?predicat CLASS
-		queryexec = GraphCreator.execQuery(RequestBuilder.A1_R2(classe, limit));
+		// Afficher ?class ?predicat CLASS
+		queryexec = GraphCreator.execQuery(RequestBuilder.A1_R2(CLASS, LIMIT));
 		res = queryexec.execSelect();
 		while (res.hasNext()) {
 			QuerySolution sol = res.next();
@@ -153,7 +154,7 @@ public class GraphCreator {
 			Edge predicat = new Edge(url_pre, s_pre, "T-Box");
 			
 			// La classe en relation via le predicat a la classe source
-			RDFNode cla = sol.get("otherclass");
+			RDFNode cla = sol.get("class");
 			String url_cla = cla.toString();
 			String s_cla = cla.toString().split("/")[cla.toString().split("/").length-1];
 			Vertex n_cla;
@@ -166,7 +167,7 @@ public class GraphCreator {
 				
 			}
 			
-			// V�rification que l'arc n'existe pas deja avant l'ajout
+			// Verification que l'arc n'existe pas deja avant l'ajout
 			boolean alreadyExist = false;
 			if (GraphCreator.graph.containsVertex(n_cla) && GraphCreator.graph.containsVertex(n_classeSource)) {
 				for(Edge e : GraphCreator.graph.findEdgeSet(n_cla, n_classeSource)) {
@@ -178,6 +179,82 @@ public class GraphCreator {
 			}
 			if (!alreadyExist) {
 				GraphCreator.graph.addEdge(predicat, n_cla, n_classeSource);
+			}
+		}
+		
+		// Afficher ?predicat rdfs:domain CLASS .
+		queryexec = GraphCreator.execQuery(RequestBuilder.A1_R3(CLASS, LIMIT));
+		res = queryexec.execSelect();
+		while (res.hasNext()) {
+			QuerySolution sol = res.next();
+			
+			// rdfs:domain
+			String url_domain = "http://www.w3.org/2000/01/rdf-schema#domain";
+			Edge domain = new Edge(url_domain, "rdfs:domain", "T-Box");
+			
+			// Un predicat dont le domaine est CLASS
+			RDFNode pre = sol.get("predicat");
+			String url_pre = pre.toString();
+			String s_pre = pre.toString().split("/")[pre.toString().split("/").length-1];
+			Vertex n_pre;
+			if (GraphCreator.nodeMap.containsKey(url_pre)) {
+				n_pre = GraphCreator.nodeMap.get(url_pre);
+			}
+			else {
+				n_pre = new Vertex(url_pre, s_pre, Color.GRAY);
+				GraphCreator.nodeMap.put(url_pre, n_pre);
+			}
+			
+			// Verification que l'arc n'existe pas deja avant l'ajout
+			boolean alreadyExist = false;
+			if (GraphCreator.graph.containsVertex(n_pre) && GraphCreator.graph.containsVertex(n_classeSource)) {
+				for(Edge e : GraphCreator.graph.findEdgeSet(n_pre, n_classeSource)) {
+					if (e.getURL().equals(url_domain)) {
+						alreadyExist=true;
+						break;
+					}
+				}
+			}
+			if (!alreadyExist) {
+				GraphCreator.graph.addEdge(domain, n_pre, n_classeSource);
+			}
+		}
+		
+		// Afficher ?predicat rdfs:range CLASS .
+		queryexec = GraphCreator.execQuery(RequestBuilder.A1_R4(CLASS, LIMIT));
+		res = queryexec.execSelect();
+		while (res.hasNext()) {
+			QuerySolution sol = res.next();
+			
+			// rdfs:domain
+			String url_range = "http://www.w3.org/2000/01/rdf-schema#range";
+			Edge range = new Edge(url_range, "rdfs:range", "T-Box");
+			
+			// Un predicat dont le range est CLASS
+			RDFNode pre = sol.get("predicat");
+			String url_pre = pre.toString();
+			String s_pre = pre.toString().split("/")[pre.toString().split("/").length-1];
+			Vertex n_pre;
+			if (GraphCreator.nodeMap.containsKey(url_pre)) {
+				n_pre = GraphCreator.nodeMap.get(url_pre);
+			}
+			else {
+				n_pre = new Vertex(url_pre, s_pre, Color.GRAY);
+				GraphCreator.nodeMap.put(url_pre, n_pre);
+			}
+			
+			// Verification que l'arc n'existe pas deja avant l'ajout
+			boolean alreadyExist = false;
+			if (GraphCreator.graph.containsVertex(n_pre) && GraphCreator.graph.containsVertex(n_classeSource)) {
+				for(Edge e : GraphCreator.graph.findEdgeSet(n_pre, n_classeSource)) {
+					if (e.getURL().equals(url_range)) {
+						alreadyExist=true;
+						break;
+					}
+				}
+			}
+			if (!alreadyExist) {
+				GraphCreator.graph.addEdge(range, n_pre, n_classeSource);
 			}
 		}
 	}
@@ -204,9 +281,9 @@ public class GraphCreator {
 			QuerySolution sol = res.next();
 			
 			// Un ancetre de la classe source
-			RDFNode parentclass = sol.get("parentclass");
-			String url_parent = parentclass.toString();
-			String s_parent = parentclass.toString().split("/")[parentclass.toString().split("/").length-1];
+			RDFNode parent = sol.get("parent");
+			String url_parent = parent.toString();
+			String s_parent = parent.toString().split("/")[parent.toString().split("/").length-1];
 			Vertex n_parent;
 			if (GraphCreator.nodeMap.containsKey(url_parent)) {
 				n_parent = GraphCreator.nodeMap.get(url_parent);
@@ -217,9 +294,9 @@ public class GraphCreator {
 			}
 			
 			// Le pere de cet ancetre
-			RDFNode superclass = sol.get("superclass");
-			String url_super = superclass.toString();
-			String s_super = superclass.toString().split("/")[superclass.toString().split("/").length-1];
+			RDFNode superc = sol.get("super");
+			String url_super = superc.toString();
+			String s_super = superc.toString().split("/")[superc.toString().split("/").length-1];
 			Vertex n_super;
 			if (GraphCreator.nodeMap.containsKey(url_super)) {
 				n_super = GraphCreator.nodeMap.get(url_super);
@@ -230,7 +307,7 @@ public class GraphCreator {
 				
 			}
 			
-			// V�rification que l'arc n'existe pas deja avant l'ajout
+			// Verification que l'arc n'existe pas deja avant l'ajout
 			boolean alreadyExist = false;
 			if (GraphCreator.graph.containsVertex(n_parent) && GraphCreator.graph.containsVertex(n_super)) {
 				for(Edge e : GraphCreator.graph.findEdgeSet(n_parent, n_super)) {
@@ -252,9 +329,9 @@ public class GraphCreator {
 			QuerySolution sol = res.next();
 			
 			// Un descendant de la classe source
-			RDFNode subclass = sol.get("subclass");
-			String url_sub = subclass.toString();
-			String s_sub = subclass.toString().split("/")[subclass.toString().split("/").length-1];
+			RDFNode sub = sol.get("sub");
+			String url_sub = sub.toString();
+			String s_sub = sub.toString().split("/")[sub.toString().split("/").length-1];
 			Vertex n_sub;			
 			if (GraphCreator.nodeMap.containsKey(url_sub)) {
 				n_sub = GraphCreator.nodeMap.get(url_sub);
@@ -265,9 +342,9 @@ public class GraphCreator {
 			}
 			
 			// Le pere de cet ancetre
-			RDFNode superclass = sol.get("superclass");
-			String url_super = superclass.toString();
-			String s_super = superclass.toString().split("/")[superclass.toString().split("/").length-1];
+			RDFNode superc = sol.get("super");
+			String url_super = superc.toString();
+			String s_super = superc.toString().split("/")[superc.toString().split("/").length-1];
 			Vertex n_super;
 			if (GraphCreator.nodeMap.containsKey(url_super)) {
 				n_super = GraphCreator.nodeMap.get(url_super);
@@ -278,7 +355,7 @@ public class GraphCreator {
 				
 			}
 			
-			// V�rification que l'arc n'existe pas deja avant l'ajout
+			// Verification que l'arc n'existe pas deja avant l'ajout
 			boolean alreadyExist = false;
 			if (GraphCreator.graph.containsVertex(n_sub) && GraphCreator.graph.containsVertex(n_super)) {
 				for(Edge e : GraphCreator.graph.findEdgeSet(n_sub, n_super)) {
@@ -311,13 +388,13 @@ public class GraphCreator {
 			GraphCreator.nodeMap.put(classe, n_classe);
 		}
 		
-		// Afficher les predicats de la A-Box les plus instanci�es par des instances de la classe source
+		// Afficher les predicats de la A-Box les plus instancies par des instances de la classe source
 		QueryExecution queryexec = GraphCreator.execQuery(RequestBuilder.A3(classe, limit));
 		ResultSet res = queryexec.execSelect();
 		while (res.hasNext()) {
 			QuerySolution sol = res.next();
 			
-			// Le nombre d'instances qui instancie le pr�dicat
+			// Le nombre d'instances qui instancie le predicat
 			RDFNode count = sol.get("count");
 			String temp = count.toString();
 			String s_count = "";
@@ -328,7 +405,7 @@ public class GraphCreator {
 			}
 			Edge n_count = new Edge("", s_count,"A-Box");
 			
-			// Le predicat instanci�
+			// Le predicat instancie
 			RDFNode pre = sol.get("predicat");
 			String url_pre = pre.toString();
 			String s_pre = pre.toString().split("/")[pre.toString().split("/").length-1];
--- src/sbc/RequestBuilder.java
+++ src/sbc/RequestBuilder.java
@@ -16,112 +16,170 @@ public class RequestBuilder {
 	
 	private static String sparqlService  = "http://dbpedia.org/sparql";
 	
-	
-	public static Query A1_R1(String classe , int limit){
+	/**
+	 * Query pour avoir les classes en relations avec la classe CLASS
+	 * CLASS ?predicat ?class .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A1_R1(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?predicat ?otherclass\n");
+		sb.append("SELECT DISTINCT ?predicat ?class\n");
 		sb.append("WHERE{\n");
-		sb.append("<"+ classe +"> ?predicat ?otherclass.\n");
-		//sb.append("?otherclass rdf:type owl:Class .\n"); // si on veux tout prendre rafinage possible
-		sb.append("FILTER (?predicat != rdfs:label ).\n");
-		sb.append("FILTER (?predicat != rdfs:comment).\n");
+		sb.append("<"+ CLASS +"> ?predicat ?class .\n");
+		sb.append("?class rdf:type owl:Class .\n");
 		sb.append("}\n");
-		sb.append("GROUP BY ?predicat ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("GROUP BY ?predicat ?class\n");
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A1_R2(String classe , int limit){
+	/**
+	 * Query pour avoir les classes en relations avec la classe CLASS
+	 * ?class ?predicat CLASS .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A1_R2(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?predicat ?otherclass\n");
+		sb.append("SELECT DISTINCT ?predicat ?class\n");
 		sb.append("WHERE{\n");
-		sb.append("?otherclass ?predicat <"+ classe +">.\n");
-		//sb.append("?otherclass rdf:type owl:Class .\n"); // si on veux tout prendre rafinage possible
-		sb.append("FILTER (?predicat != rdf:type) .\n");
+		sb.append("?class ?predicat <"+ CLASS +"> .\n");
+		sb.append("?class rdf:type owl:Class .\n");
 		sb.append("}\n");
-		sb.append("GROUP BY ?predicat ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("GROUP BY ?predicat ?class\n");
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A2_R1(String classe , int limit){
+	/**
+	 * Query pour avoir les predicats dont le domaine est la classe CLASS
+	 * ?predicat rdfs:domain CLASS .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A1_R3(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?parentclass ?superclass\n");
+		sb.append("SELECT DISTINCT ?predicat\n");
 		sb.append("WHERE{\n");
-		sb.append("<"+ classe +"> (rdfs:subClassOf)* ?parentclass .\n");
-		//sb.append("?otherclass rdf:type owl:Class .\n"); // si on veux tout prendre rafinage possible
-		sb.append("?parentclass rdfs:subClassOf ?superclass .\n");
+		sb.append("?predicat rdfs:domain <"+ CLASS +"> .\n");
 		sb.append("}\n");
-		//sb.append("GROUP BY ?predicat ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A2_R1(String classe , int limit, int max){
+	/**
+	 * Query pour avoir les predicats dont le range est la classe CLASS
+	 * ?predicat rdfs:range CLASS .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A1_R4(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?parentclass ?superclass\n");
+		sb.append("SELECT DISTINCT ?predicat\n");
 		sb.append("WHERE{\n");
-		sb.append("<"+ classe +"> (rdfs:subClassOf){0,"+ max+"} ?parentclass .\n");
-		//sb.append("?otherclass rdf:type owl:Class .\n"); // si on veux tout prendre rafinage possible
-		sb.append("?parentclass rdfs:subClassOf ?superclass .\n");
+		sb.append("?predicat rdfs:range <"+ CLASS +"> .\n");
 		sb.append("}\n");
-		//sb.append("GROUP BY ?relation ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A2_R2(String classe , int limit){
+	/**
+	 * Query pour la hiérachie montante de la classe CLASS
+	 * CLASS (rdfs:subClassOf)* ?parent .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A2_R1(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?subclass ?superclass\n");
+		sb.append("SELECT DISTINCT ?parent ?super\n");
 		sb.append("WHERE{\n");
-		sb.append("?subclass (rdfs:subClassOf)+ <"+ classe +">.\n");
-		sb.append("?subclass rdfs:subClassOf ?superclass.\n");
-		sb.append("?subclass rdf:type owl:Class.\n");
-		sb.append("?superclass rdf:type owl:Class.\n"); 
+		sb.append("<"+ CLASS +"> (rdfs:subClassOf)* ?parent .\n");
+		sb.append("?parent rdfs:subClassOf ?super .\n");
 		sb.append("}\n");
-		//sb.append("GROUP BY ?predicat ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A2_R2(String classe , int limit,int  max){
+	public static Query A2_R1bis(String CLASS, int N){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT ?subclass ?superclass\n");
+		sb.append("SELECT DISTINCT ?parent ?super\n");
 		sb.append("WHERE{\n");
-		sb.append("?subclass (rdfs:subClassOf){1,"+ max +"} <"+ classe +">.\n");
-		sb.append("?subclass rdfs:subClassOf ?superclass.\n");
-		sb.append("?subclass rdf:type owl:Class.\n");
-		sb.append("?superclass rdf:type owl:Class.\n"); 
+		sb.append("<"+ CLASS +"> (rdfs:subClassOf){0,"+ N +"} ?parent .\n");
+		sb.append("?parent rdfs:subClassOf ?super .\n");
 		sb.append("}\n");
-		//sb.append("GROUP BY ?relation ?otherclass\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query A3(String classe , int limit){
+	/**
+	 * Query pour la hiérachie descendante de la classe CLASS
+	 * ?sub (rdfs:subClassOf)+ CLASS .
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A2_R2(String CLASS, int LIMIT){
+		StringBuilder sb = new StringBuilder();
+		sb.append(PREFIX);
+		sb.append("SELECT DISTINCT ?sub ?super\n");
+		sb.append("WHERE{\n");
+		sb.append("?sub (rdfs:subClassOf)+ <"+ CLASS +"> .\n");
+		sb.append("?sub rdfs:subClassOf ?super.\n");
+		sb.append("}\n");
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
+		
+		return QueryFactory.create(sb.toString());
+	}
+	
+	public static Query A2_R2bis(String CLASS, int N){
+		StringBuilder sb = new StringBuilder();
+		sb.append(PREFIX);
+		sb.append("SELECT DISTINCT ?sub ?super\n");
+		sb.append("WHERE{\n");
+		sb.append("?sub (rdfs:subClassOf){1,"+ N +"} <"+ CLASS +"> .\n");
+		sb.append("?sub rdfs:subClassOf ?super.\n");
+		sb.append("}\n");
+		//System.out.println(sb.toString());
+		
+		return QueryFactory.create(sb.toString());
+	}
+	
+	/**
+	 * 
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query A3(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
 		sb.append("SELECT (COUNT(?otherinstance) AS ?count) ?predicat\n");
 		sb.append("WHERE{\n");
-			sb.append("?instance rdf:type <"+ classe +">.\n");
+			sb.append("?instance rdf:type <"+ CLASS +"> .\n");
 			sb.append("{\n");
 				sb.append("?instance ?predicat ?otherinstance .\n");
 				sb.append("?otherinstance rdf:type ?class .\n");
@@ -135,58 +193,71 @@ public class RequestBuilder {
 		sb.append("}\n");
 		sb.append("GROUP BY ?predicat\n");
 		sb.append("ORDER BY DESC(?count)\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query B1(String classe , int limit ){
+	/**
+	 * Query pour chercher les sous-classes de CLASS et leurs instances associées
+	 * @param CLASS
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query B1(String CLASS , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
-		sb.append("SELECT DISTINCT(COUNT(?instance) AS ?count) ?subclass ?superclass \n");
+		sb.append("SELECT DISTINCT ?sub ?super (COUNT(?instance) AS ?count) \n");
 		sb.append("WHERE{\n");
-			sb.append("?subclass (rdfs:subClassOf)* <"+ classe +"> .\n");
-			sb.append("?subclass rdfs:subClassOf ?superclass .\n");
-			sb.append("?instance rdf:type ?subclass .\n");
-			sb.append("?subclass rdf:type owl:Class .\n");
-			sb.append("?superclass rdf:type owl:Class .\n");
+		sb.append("?sub (rdfs:subClassOf)* <"+ CLASS +"> .\n");
+		sb.append("?sub rdfs:subClassOf ?super .\n");
+		sb.append("?instance rdf:type ?sub .\n");
 		sb.append("}\n");
-		sb.append("GROUP BY ?subclass ?superclass\n");
-		sb.append("ORDER BY DESC( ?count)\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		
-		System.out.println(sb.toString());
+		sb.append("GROUP BY ?sub ?super\n");
+		sb.append("ORDER BY DESC(?count)\n");
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query C1(String ressource , int limit){
+	/**
+	 * Query pour chercher les instances qui sont objets d'un prédicat avec la RESSOURCE comme sujet
+	 * @param RESSOURCE
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query C1(String RESSOURCE , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
 		sb.append("SELECT DISTINCT ?class\n");
 		sb.append("WHERE{\n");
-			sb.append("<"+ ressource +"> rdf:type ?class .\n");
-			
+		sb.append("<"+ RESSOURCE +"> rdf:type ?class .\n");
 		sb.append("}\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
 	
-	public static Query C2(String ressource , int limit){
+	/**
+	 * Query pour chercher les instances qui sont sujets d'un prédicat avec la RESSOURCE comme objet
+	 * @param RESSOURCE
+	 * @param LIMIT
+	 * @return
+	 */
+	public static Query C2(String RESSOURCE , int LIMIT){
 		StringBuilder sb = new StringBuilder();
 		sb.append(PREFIX);
 		sb.append("SELECT DISTINCT ?predicat ?instance\n");
 		sb.append("WHERE{\n");
-			sb.append("<"+ ressource +"> ?predicat ?instance .\n");
-			sb.append("?instance rdf:type ?classs .\n");
-			//sb.append("?class rdf:type owl:Class  .\n");
+		sb.append("<"+ RESSOURCE +"> ?predicat ?instance .\n");
+		sb.append("?instance rdf:type ?classs .\n");
+		sb.append("?class rdf:type owl:Class .\n");
 		sb.append("}\n");
-		sb.append("LIMIT "+limit+" OFFSET 0\n");
-		System.out.println(sb.toString());
+		sb.append("LIMIT "+LIMIT+" OFFSET 0\n");
+		//System.out.println(sb.toString());
 		
 		return QueryFactory.create(sb.toString());
 	}
@@ -200,15 +271,20 @@ public class RequestBuilder {
 	  }
 	}
 
+	/**
 	public static void main(String[] args) {
 		//RunQuery(A1_R1("http://dbpedia.org/ontology/CelestialBody", 100));
 		//RunQuery(A1_R2("http://dbpedia.org/ontology/CelestialBody", 100));
-		RunQuery(A2_R1("http://dbpedia.org/ontology/CelestialBody", 100));
+		//RunQuery(A1_R3("http://dbpedia.org/ontology/CelestialBody", 100));
+		//RunQuery(A1_R4("http://dbpedia.org/ontology/CelestialBody", 100));
+		//RunQuery(A2_R1("http://dbpedia.org/ontology/CelestialBody", 100));
+		//RunQuery(A2_R1bis("http://dbpedia.org/ontology/CelestialBody", 100));
 		//RunQuery(A2_R2("http://dbpedia.org/ontology/CelestialBody", 1000));
+		//RunQuery(A2_R2bis("http://dbpedia.org/ontology/CelestialBody", 1000));
 		//RunQuery(A3("http://dbpedia.org/ontology/Planet", 100));
 		//RunQuery(B1("http://dbpedia.org/ontology/CelestialBody", 100));
 		//RunQuery(C1("http://dbpedia.org/resource/Naboo", 100));
 		//RunQuery(C2("http://dbpedia.org/resource/Naboo", 100));
-	}
+	}*/
 	
 }
